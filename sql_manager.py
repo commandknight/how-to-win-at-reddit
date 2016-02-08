118eee0aa38be8e9a2c73cdfd3fe4c8375e9e78f
Jeet Nagda <jeetnagda@gmail.com>
2016-02-08 14:17:15 -0800

    Updated sql_manager.py as public API

--- sql_manager.py
+++ sql_manager.py
@@ -7,13 +7,15 @@ Jeet Nagda, Timothie Fujita, Jocelyne Perez
 
 import sqlite3
 
+
 def get_test_data():
     """
-
-    :return:
+    Returns first 10 rows of May2015 table as a sample test
     """
     conn = sqlite3.connect('/Users/jnagda/Documents/Reddit_Comments/database.sqlite')
-    return conn.execute('SELECT * FROM May2015 LIMIT 10')
+    result = conn.execute('SELECT * FROM May2015 LIMIT 10')
+    conn.close()
+    return result
 
 
 def print_test():
