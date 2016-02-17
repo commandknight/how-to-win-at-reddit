33e651f45658b41f2fa679a53199efdc1f57fbd5
Jeet Nagda <jeetnagda@gmail.com>
2016-02-16 16:25:31 -0800

    Adding AWS test python script

---
+++ test.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+config = {
+    'user': 'commandknight',
+    'password': 'paper2mate',
+    'host': 'cs175redditproject.cxayrrely1fe.us-west-2.rds.amazonaws.com',
+    'database': 'cs175reddit',
+    'raise_on_warnings': True,
+}
+
+cnx = mysql.connector.connect(**config)
+cursor = cnx.cursor()
+
+query = ("SELECT * FROM testTable")
+
+cursor.execute(query)
+
+for (id_test) in cursor:
+    print(id_test[0])
+
+cursor.close()
+cnx.close()
