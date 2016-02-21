67f0f148c8077fb5805ad517b8e61a7af7fc87ea
Jeet Nagda <jeetnagda@gmail.com>
2016-02-21 10:09:36 -0800

    crappy decision tree clf

--- text_clean_test.py
+++ text_clean_test.py
@@ -2,7 +2,7 @@ import mysql_manager
 import serialize_comments
 import sql_manager
 
-sql_statement = "SELECT childrenComments FROM ParentPostDetails WHERE parentPost_id=\'t3_2lcfzp\' LIMIT 10"
+sql_statement = "SELECT parentPost_id,childrenComments,score FROM ParentPostDetails LIMIT 20"
 
 sql_statement_children = "SELECT body FROM May2015 WHERE id = "
 
@@ -29,56 +29,11 @@ def tokenize_simple(document):
 
 
 def remove_stopwords_inner(tokens, stopwords):
-    """
-    Removes stopwords from the given tokens.
-
-    Parameters
-    ----------
-    tokens : Iterable[str]
-        The tokens from which to remove stopwords.
-
-    stopwords : Iterable[str]
-        The tokens to remove.
-
-    Returns
-    -------
-    List[str]
-        The input tokens with stopwords removed.
-
-    Example
-    -------
-    >>> document = load_hamlet()
-    >>> tokens = tokenize_simple(document)
-    >>> remove_stopwords_inner(tokens, ['of', 'to'])[:10]
-    ['the', 'tragedie', 'hamlet', 'by', 'william', 'shakespeare', '1599', 'actus', 'primus', 'scoena']
-    """
-    # convert to a set for speed of lookup in the next step
     stopwords = set(stopwords)
     return [x for x in tokens if x not in stopwords]
 
 
 def remove_stopwords_nltk(tokens):
-    """
-    Remove the NLTK English stopwords from the given tokens using remove_stopwords_inner.
-
-    Parameters
-    ----------
-    tokens : Iterable[str]
-        The tokens from which to remove stopwords.
-
-    Returns
-    -------
-    List[str]
-        The input tokens with NLTK's English stopwords removed.
-
-    Example
-    -------
-    >>> document = load_hamlet()
-    >>> tokens = tokenize_nltk(document)
-    >>> tokens = remove_stopwords_nltk(tokens)
-    >>> tokens[:10]
-    ['[', 'tragedie', 'hamlet', 'william', 'shakespeare', '1599', ']', 'actus', 'primus', '.']
-    """
     from nltk.corpus import stopwords
     return remove_stopwords_inner(tokens, stopwords=stopwords.words('english'))
 
@@ -113,11 +68,46 @@ I am eating pancakes with maple syrup Just joined the bitcoin and changetip grou
 Hello, finally trying bitcoin after hearing so much about it :) thanksI'm trying this ... Just getting back into Cryptocurrency. Free bits please?Hi! I'm new to Bitcoin does this tip bot work the same as doge?
 """
 
+
+def decision_tree_pipeline(train_data, train_targets, test_data):
+    from sklearn.pipeline import Pipeline
+    from sklearn.feature_extraction.text import CountVectorizer
+    from nltk.corpus import stopwords
+    from sklearn.feature_extraction.text import TfidfTransformer
+    from sklearn.tree import DecisionTreeClassifier
+    text_clf_bernNB = Pipeline([('vect', CountVectorizer(stop_words=stopwords.words('english'))),
+                                ('tfidf', TfidfTransformer()),
+                                ('clf', DecisionTreeClassifier()),
+                                ])
+    text_clf_bernNB = text_clf_bernNB.fit(train_data, train_targets)
+    predicted_bernNB = text_clf_bernNB.predict(test_data)
+    return predicted_bernNB
+
+
+
 if __name__ == '__main__':
     q_results = mysql_manager.perform_query(sql_statement)
     big_string = ""
-    for x in q_results:
-        list_of_children = serialize_comments.deserialize_list(x[0])
+    train_data = []
+    train_targets = []
+    test_data = []
+    test_targets = []
+    for id, comments, score in q_results[:10]:
+        list_of_children = serialize_comments.deserialize_list(comments)
         # print(list_of_children)
         big_string = get_all_text_from_children_comments(list_of_children)
-    process_big_text(big_string)
+        train_data.append(big_string)
+        if score > 100:
+            train_targets.append(1)
+        else:
+            train_targets.append(0)
+    for id, comments, score in q_results[10:]:
+        list_of_children = serialize_comments.deserialize_list(comments)
+        # print(list_of_children)
+        big_string = get_all_text_from_children_comments(list_of_children)
+        test_data.append(big_string)
+        if score > 100:
+            test_targets.append(1)
+        else:
+            test_targets.append(0)
+    x = decision_tree_pipeline(train_data, train_targets, test_data)
