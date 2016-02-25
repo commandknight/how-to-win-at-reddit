e871827cdd2d76d9a7f40b1591754b41437c1dd1
Jeet Nagda <jeetnagda@gmail.com>
2016-02-24 16:29:07 -0800

    working on fetching lots of SQL rows, and some reorg

--- text_pipeline/comment_pipeline.py
+++ text_pipeline/comment_pipeline.py
@@ -7,11 +7,11 @@ name: process_child_comments_pipeline()
 Create a method that given a parent post ID, will get all the children posts up to a time limit X and return array of ids
 name: get_children_commentsIDs()
 """
+import time
 
-import mysql_manager
-import sql_manager
-
-from text_pipeline import serialize_comments
+from text_pipeline import comment_db_manager
+from text_pipeline import mysql_manager
+from text_pipeline import parent_post_pipeline
 
 
 def process_child_comments_pipeline(parentPost_id, comment_db_path=None, time_limit=180):
@@ -20,17 +20,18 @@ def process_child_comments_pipeline(parentPost_id, comment_db_path=None, time_li
     :param parentPost_id: required - Parent post ID to be searched
     :param comment_db_path: optional - db path for search, will default to Timothie's machine
     :param time_limit: optional - time limit to look for comments in minutes
-    :return: nothing
+    :return: list of children comment IDs within time limit
     """
-    # parent_db_connection = mysql_manager
-    parent_db_connection = None
-    children_list = get_children_commentIDs(comment_db_path, parent_db_connection, parentPost_id, time_limit)
-    print(children_list)
-    parent_update(parent_db_connection, parentPost_id, children_list)
-    #parent_db_connection.close_connection()
+    parent_db_connection = mysql_manager
+    curr = parent_db_connection.create_cursor()
+    children_list = get_children_commentIDs(curr, comment_db_path, parent_db_connection, parentPost_id, time_limit)
+    mysql_manager.update_parentPost(curr, children_list, parentPost_id)
+    parent_db_connection.close_connection(curr)
+
+    return children_list
 
 
-def get_children_commentIDs(comment_path, parent_connect, parentPost_id, time_limit):
+def get_children_commentIDs(curr, comment_path, parent_connect, parentPost_id, time_limit):
     """
     Get children post IDs made within time_limit from when parentPost_id is made.
     :param comment_path: path to local comment db to be connected to
@@ -42,34 +43,30 @@ def get_children_commentIDs(comment_path, parent_connect, parentPost_id, time_li
     children_ids = []
     parent_post_time = 0
     time_limit_epoch = time_limit * 60
+
     parent_query = ('SELECT parentPost_id, timecreated_utc FROM ParentPostDetails WHERE parentPost_id = \'', parentPost_id, '\'')
     children_query = ('SELECT id, parent_id, link_id, created_utc FROM May2015 WHERE link_id = \'', parentPost_id, '\'')
     parent_query_string = ''.join(parent_query)
     children_query_string = ''.join(children_query)
-    parent_post_info = mysql_manager.perform_query(parent_query_string)
+
+    parent_post_info = parent_connect.perform_query(curr, parent_query_string)
     children = query_comment_db(comment_path, children_query_string)
+
     for(parentPost_id, timecreated_utc) in parent_post_info:
         parent_post_time = int(float(timecreated_utc))
+
     cutoff_time_limit = parent_post_time + time_limit_epoch
+    p_time = time.strftime('%m-%d-%Y %H:%M:%S', time.localtime(parent_post_time))
+    c_time = 0;
+
     for x,y,z,a in children:
-        children_ids.append(x)
-        # if a >= parent_post_time and a <= cutoff_time_limit:
-        #     children_ids.append(x)
-        #     print(x, y, z)
+        if a < cutoff_time_limit:
+            children_ids.append(x)
+            c_time = time.strftime('%m-%d-%Y %H:%M:%S', time.localtime(a))
+
+    print("Total children: " + str(len(children)) + " | After time cutoff: " + str(len(children_ids)))
+    print("Parent post time: " + str(p_time) + " | Last child within time limit: " + str(c_time))
     return children_ids
-
-
-def parent_update(parent_connect, parentPost_id, children_ids):
-    """
-    Update given parentPost with its serialized list of children_ids
-    :param parent_connect: connection to mysql with parent post information
-    :param parentPost_id: id of parent post to update
-    :param children_ids: list of children ids
-    :return: none
-    """
-    sl = serialize_comments
-    serialized_children = sl.serialize_list(children_ids)
-    mysql_manager.update_parentPost(serialized_children, parentPost_id)
 
 
 def query_comment_db(db_path, query):
@@ -80,30 +77,27 @@ def query_comment_db(db_path, query):
     :return: list of comment db query result
     """
     query_results = []
-    reddit_db = sql_manager
+    reddit_db = comment_db_manager
 
     if db_path is None:
-        db_path = reddit_db.timothie_path
+        db_path = reddit_db.timothie_desktop
 
-    query_results = reddit_db.perform_query(db_path, query)
+    conn = reddit_db.open_db_connection(db_path)
+    query_results = reddit_db.perform_query(conn, query)
+    reddit_db.close_db_connection(conn)
     return query_results
 
 
 if __name__ == '__main__':
-    q = ('SELECT parentPost_id, title FROM ParentPostDetails LIMIT 2000')
-    # my = mysql_manager
-    q_results = mysql_manager.perform_query(q)
-    #my.close_connection()
+    parent_id = 't3_37y5rx', 'AskReddit'
+    parent_dict = []
+    parent = parent_post_pipeline.get_parentpost_dict(parent_id)
+    print(parent)
+    parent_dict.append(parent)
+    mysql_manager.insert_parentdetails_BIG(parent_dict)
+    process_child_comments_pipeline('t3_37y5rx')
 
-    for x,y in q_results:
-        print(x + " | " + y)
-        process_child_comments_pipeline(x)
-    #process_child_comments_pipeline('t3_34gitq')
-    #process_child_comments_pipeline('t3_10v5wy')
-    #process_child_comments_pipeline('t3_10v5wy')
-    #res = mysql_manager.perform_query('SELECT timecreated_utc FROM ParentPostDetails WHERE parentPost_id = \'t3_36i6nn\'')
-    #print(res)
-    #res = mysql_manager.perform_query('SELECT parentPost_id, COUNT(*) FROM ParentPostDetails GROUP BY parentPost_id HAVING COUNT(*) > 1')
-    #for x in res:
-    #    print(x)
-    #process_child_comments_pipeline('t3_36i6nn')
+    # process_child_comments_pipeline('t3_37zlq2')
+    # process_child_comments_pipeline('t3_37w2go')
+    # process_child_comments_pipeline('t3_37zyk6')
+    # process_child_comments_pipeline('t3_37yawp')
